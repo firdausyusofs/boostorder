86eb0bad69dea93a411ef421fd13a82c4e74a85c
Firdaus Yusof <firdausyusof06@gmail.com>
2021-09-26 02:00:38 +0800

    - Added catalog page

---
+++ resources/views/catalog.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boostorder - Catalog</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset("css/app.css") }}" rel="stylesheet" />
+</head>
+<body>
+    <div class="catalog_wrapper">
+        <h1 class="main_title">Products</h1>
+        
+        <div class="product_holder">
+            <div class="product">
+                <div class="product_img">
+                    <div class="product_price">RM 10.00</div>
+                    <button class='bulk_order_btn'>Select</button>
+                </div>
+                <h3 class="product_title">Colgate</h3>
+            </div>
+            <div class="product">
+                <div class="product_img"></div>
+                <h3 class="product_title">Colgate</h3>
+            </div>
+            <div class="product">
+                <div class="product_img"></div>
+                <h3 class="product_title">Colgate</h3>
+            </div>
+            <div class="product">
+                <div class="product_img"></div>
+                <h3 class="product_title">Colgate</h3>
+            </div>
+            <div class="product">
+                <div class="product_img"></div>
+                <h3 class="product_title">Colgate</h3>
+            </div>
+        </div>
+
+        <button class="order_btn">Order</button>
+    </div>
+</body>
+</html>
